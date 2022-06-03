bace92f8dc384625d72b02bd281ed6db4807a0b5
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-06-03 16:01:42 -0700

    connected front-end to back-end

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 require('dotenv').config()
+const path = require('path')
+
 const express = require('express')
 const server = express()
 
-const PORT = process.env.PORT || 5000
+server.use(express.json())
+server.use(express.static(path.join(__dirname, 'client/build')))
+
+
 
 server.get('/api/users', ( req,res) => {
     res.json([
         {id: 1, username: 'krystle'}
     ])
+})
+
+const PORT = process.env.PORT || 5000
+
+server.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 server.listen( PORT, () => {
