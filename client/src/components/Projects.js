bc50d8fd4929b7704c7b020f740e58082dc8b6c0
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-06-03 16:43:31 -0700

    Project Components CSS

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -9,24 +9,31 @@ return (
         </h1> 
 
         <div className="containerBody">
-            <img src={props.img} alt="" />
+
+           <img src="https://images.unsplash.com/photo-1518310952931-b1de897abd40?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742" alt="" />
             <h2 className="card-title">Gym Ratz</h2>
             <p className="card_info">Get off your couch, and join Gym Ratz anywhere in the world</p>
-            <buttom className="card__btn">View Project</buttom>
-        </div>
-        <div className="containerBody">
+            <button className="card__btn">View Project</button>
+       </div>
+        {/* <div className="containerBody">
+            <div classname="img-card">
+
             <img src="https://images.unsplash.com/photo-1518310952931-b1de897abd40?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742" alt="" />
+            </div>
             <h2 className="card-title">Gym Ratz</h2>
             <p className="card_info">Get off your couch, and join Gym Ratz anywhere in the world</p>
-            <buttom className="card__btn">View Project</buttom>
-        </div>
+            <button className="card__btn">View Project</button>
+       </div>
         <div className="containerBody">
+        <div classname="img-card">
+
             <img src="https://images.unsplash.com/photo-1518310952931-b1de897abd40?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742" alt="" />
+        </div>
             <h2 className="card-title">Gym Ratz</h2>
             <p className="card_info">Get off your couch, and join Gym Ratz anywhere in the world</p>
-            <buttom className="card__btn">View Project</buttom>
-        </div>
-        
+            <button className="card__btn">View Project</button>
+       </div> */}
+      
 </div>
 )
 }
