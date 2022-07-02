c09502e713f7739d83ab7e94f3f0a558cabc30d2
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-07-02 16:20:59 -0700

    WIP

--- client/src/components/projects/home/Home.js
+++ client/src/components/projects/home/Home.js
@@ -6,17 +6,17 @@ const Home = () => {
   return (
     <div className="pages">
           <div className='content'>
+     <div className='header'>
+     <h2>Welcome to The Home Page</h2>
      
-        <h2>Welcome to The Home Page</h2>
-      
-  
+     </div>
+   
           <div className='card'>
-            <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80" alt="Projects" />
-            <div class="container">
-              <h4><b>Projects</b></h4>
-              <p> Current Projects I am working on</p>
-              
-            </div>
+          <h4>Projects</h4>
+          <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80" alt="Projects" />
+          <div class="container">
+            <p> Projects I am currently working on</p>
+           </div>
           </div>
       </div>
        
