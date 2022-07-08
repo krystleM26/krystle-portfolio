45f7b0977d3a58bbc4d51d141b1c2fca5c1960c9
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-07-07 18:28:18 -0700

    Updates Home and Project Componenet

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -1,19 +1,20 @@
 import React from "react";
 import  './projects.css'
+import  healingInt from '../../assets/HealingInt.png'
 
 const Projects = () => {
 return (
 
 <div className="container">
     <h2>
-       Projects Page Coming Soon
+       Projects 
         </h2> 
 
         <div className="containerBody">
 
-           <img src="https://images.unsplash.com/photo-1518310952931-b1de897abd40?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742" alt="" />
-            <h2 className="card-title">Gym Ratz</h2>
-            <p className="card_info">Get off your couch, and join Gym Ratz anywhere in the world</p>
+           <img src={healingInt} alt="Healing Intentions Natural-logo" />
+            <h2 className="card-title">Healing Intentions</h2>
+            <p className="card_info">E-commerce Business</p>
             <button className="card__btn">View Project</button>
        </div> 
         <div className="containerBody">
