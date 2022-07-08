f78dccb1568d448d3fee7b0dc6e07a2df440cb66
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-07-07 18:34:44 -0700

    WIP- Projects

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -10,14 +10,15 @@ return (
        Projects 
         </h2> 
 
-        <div className="containerBody">
+        <div className="containerProject">
 
            <img src={healingInt} alt="Healing Intentions Natural-logo" />
             <h2 className="card-title">Healing Intentions</h2>
             <p className="card_info">E-commerce Business</p>
             <button className="card__btn">View Project</button>
        </div> 
-        <div className="containerBody">
+
+        <div className="containerProject">
             <div className="img-card">
 
             <img src="https://images.unsplash.com/photo-1518310952931-b1de897abd40?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742" alt="" />
@@ -26,7 +27,7 @@ return (
             <p className="card_info">Get off your couch, and join Gym Ratz anywhere in the world</p>
             <button className="card__btn">View Project</button>
        </div>
-        <div className="containerBody">
+        <div className="containerProject">
         <div className="img-card">
 
             <img src="https://images.unsplash.com/photo-1518310952931-b1de897abd40?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742" alt="" />
