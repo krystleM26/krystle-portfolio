0c759f7375b7762959ad2a841d63bc3c69375a6d
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-07-18 22:54:56 -0700

    Update

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './home.css'
+import { Link } from 'react-router-dom'
 import ProjectsImg from '../../assets/projectsImg.png'
 import headShot from '../../assets/HeadshotMe.png'
 import codeGreen from '../../assets/codeGreen.png'
@@ -9,13 +10,15 @@ const Home = () => {
     <div className="homepage">
       <div className="header">
         <div>
-
-        <h2>Greetings!</h2>
-        <div className='text-content'>
-        <p> My name is Krystle and I am a software engineer. 
-        I enjoy creating pathways across the web that helps make life easier for users around the world. </p>
-        
-        </div>
+          <h2>Greetings!</h2>
+          <div className="text-content">
+            <p>
+              {' '}
+              My name is Krystle and I am a software engineer. I enjoy creating
+              pathways across the web that helps make life easier for users
+              around the world.{' '}
+            </p>
+          </div>
         </div>
         <div>
           <img className="headShot" src={headShot} alt="My Headshot" />
@@ -25,14 +28,16 @@ const Home = () => {
         <div className="cards">
           <h2>Projects</h2>
           <img src={ProjectsImg} className="projectImg" alt="Projects" />
-
-          <button>View Projects</button>
+          <Link to="/projects">
+            <button>View Projects</button>
+          </Link>
         </div>
         <div className="cards">
           <h2>Blog</h2>
           <img src={codeGreen} className="codeGreen" alt="Projects" />
-
-          <button>View Blog</button>
+          <a href="https://medium.com/green-code">
+            <button>View Blog</button>
+          </a>
         </div>
       </div>
     </div>
--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -1,35 +1,41 @@
-import React from "react";
-import  './projects.css'
-import  healingInt from '../../assets/HealingInt.png'
+import React from 'react'
+import './projects.css'
+import healingInt from '../../assets/HealingInt.png'
 
 const Projects = () => {
-return (
+  return (
+    <div className="containerBody">
+      <h2>Projects</h2>
 
-<div className="containerBody">
-    <h2>
-       Projects 
-        </h2> 
-
-    <div className="containerProject">
+      <div className="containerProject">
         <div className="img-card">
-
-           <img src={healingInt} alt="Healing Intentions Natural-logo" />
+          <img src={healingInt} alt="Healing Intentions Natural-logo" />
         </div>
-            <h3 className="card-title">Healing Intentions</h3>
-            <p className="card_info">E-commerce Business</p>
-            <button className="card__btn">View Project</button>
-       </div> 
+        <h3 className="card-title">Healing Intentions</h3>
+        <p className="card_info">E-commerce Business</p>
+        <a href="https://github.com/krystleM26/hi-app" className="card_btn">
+          {' '}
+          <button>View Project</button>
+        </a>
+      </div>
 
-     <div className="containerProject">
-            <div className="img-card">
-
-            <img src="https://images.unsplash.com/photo-1518310952931-b1de897abd40?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742" alt="" />
-            </div>
-            <h3 className="card-title">Gym Ratz</h3>
-            <p className="card_info">Get off your couch, and join Gym Ratz anywhere in the world</p>
-            <button className="card__btn">View Project</button>
-       </div>
-    {/* <div className="containerProject">
+      <div className="containerProject">
+        <div className="img-card">
+          <img
+            src="https://images.unsplash.com/photo-1518310952931-b1de897abd40?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742"
+            alt=""
+          />
+        </div>
+        <h3 className="card-title">Gym Ratz</h3>
+        <p className="card_info">
+          Get off your couch, and join Gym Ratz anywhere in the world
+        </p>
+        <a href="https://gym-ratz.herokuapp.com/" className="card__btn">
+          {' '}
+          <button>View Project </button>
+        </a>
+      </div>
+      {/* <div className="containerProject">
         <div className="img-card">
 
             <img src="https://images.unsplash.com/photo-1518310952931-b1de897abd40?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742" alt="" />
@@ -38,9 +44,8 @@ return (
             <p className="card_info">Get off your couch, and join Gym Ratz anywhere in the world</p>
             <button className="card__btn">View Project</button>
        </div> */}
-      
-</div>
-)
+    </div>
+  )
 }
 
-export default Projects 
+export default Projects
