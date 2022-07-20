28517f72cd76bd7c8c5f36bda4b87b5e629daaa1
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-07-20 12:56:10 -0700

    Update CSS

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -6,29 +6,30 @@ import KrystleResume from '../../assets/KrystleMitchell-Resume.pdf'
 
 const Home = () => {
   return (
-    <div className="homepage">
-         <div className='top-content'>
-      <div className="header">
-     
-          <h2>Greetings!</h2>  
+    <div className>
+      <div className="top-content">
+        <div className="header">
+          <h2>Greetings!</h2>
+          <div className="text-content">
             <p>
               {' '}
               My name is Krystle and I am a software engineer. I enjoy creating
               pathways across the web that helps make life easier for users
               around the world.{' '}
             </p>
-         
+          </div>
+          <div className="btn">
+        <Link to="/about">
+          <button>Learn More About Me</button>
+        </Link>
+      </div>
         </div>
         <div className="headShot">
-          <img  src={headShot} alt="My Headshot" />
+          <img src={headShot} alt="My Headshot" />
         </div>
+        
       </div>
-        <div className='btn'>
-          
-          <Link to="/about"><button>
-          Learn More About Me
-            </button></Link>
-        </div>
+      
       <div className="card">
         <div className="cards">
           <h2>Projects</h2>
@@ -36,7 +37,7 @@ const Home = () => {
             <button>View</button>
           </Link>
         </div>
-        <div className='cards'>
+        <div className="cards">
           <h2>Resume</h2>
           <Link to={KrystleResume} target="_blank" download>
             <button>Download</button>
