b4c6fbe0ae5a719bb1fa5af54c6e6bae8a619203
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-07-28 17:17:44 -0700

    Removed Cors

--- index.js
+++ index.js
@@ -1,46 +1,24 @@
 require('dotenv').config()
 const path = require('path')
+
 const express = require('express')
-
-const { google } = require('googleapis')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mailer = require('nodemailer')
-
-// OauthClient Set up//
-
-const OAuth2 = google.auth.OAuth2
-const oauth2Client = new OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET,
-  'https://developers.google.com/oauthplayground',
-)
-oauth2Client.setCredentials({
-  refresh_token: process.env.REFRESH_TOKEN,
-})
-const accessToken = oauth2Client.getAccessToken()
-
-// Express
-
 const server = express()
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
-
 const router = express.Router()
+const cors = require('cors')
+const mailer= require('nodemailer')
+
+//  Server Set-up
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'client/build')))
 
 server.use('/', router)
-server.use(
-  cors({
-    origin: ['http://www.krystlemitchell.com/contact'],
-  }),
-)
+server.use(cors());
 
-server.get('/contact', (req, res) => {
-  res, json({ msg: 'cors enabled for contact' })
-})
+// server.get('/contact', cors(), (req,res) => {
+//   res,json({msg: 'cors enabled for contact'})
+// })
+
 
 server.get('/', (req, res) => {
   res.send('Hello World')
@@ -59,43 +37,39 @@ server.listen(PORT, () => {
 // Receive Emails Setup
 
 const transporter = mailer.createTransport({
-  host: 'smpt.gmail.com',
-  port: 9000,
-  secure: true,
-  auth: {
-    type: 'OAuth2',
-    user: process.env.G_MAIL,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
-    accessToken: accessToken
-  }
-})
+    service: 'gmail',
+    port: 9000,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    },
+  })
+  transporter.verify((error) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Ready To Send')
+    }
+  })
 
-
-transporter.verify((error) => {
-  if (error) {
-    console.log(error)
-  } else {
-    console.log('Ready To Send')
-  }
-})
 
 server.post('/contact', (req, res) => {
   const { name, email, message } = req.body
 
   const mail = {
     from: name,
-    to: 'greencodeai@gmail.com',
+    to: 'mitchell.krystle2@gmail.com',
     subject: 'Contact Form Submission',
     html: `<p> Name: ${name}</p><p> Email: ${email}</p><p>Message: ${message}</p>`,
   }
 
-  transporter.sendMail(mail, (error, res) => {
+  transporter.sendMail(mail, (error, data) => {
     if (error) {
       res.json({ status: 'ERROR' })
     } else {
       res.json({ status: 'Message Sent' })
     }
   })
+
+
 })
