b44b24aed570f52299f0d1a39ac39f39c6985656
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-10-19 19:36:21 -0700

    Frontend and backend are working

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
+const path = require('path')
 const bodyParser = require('body-parser')
-const dotenv = require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 //Middleware
 
@@ -11,7 +15,15 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from server!' })
+})
+
 const PORT = process.env.PORT || 5000
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', index.html))
+})
+
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT:${PORT}...`)
+  console.log(`Listening on ${PORT}...`)
 })
