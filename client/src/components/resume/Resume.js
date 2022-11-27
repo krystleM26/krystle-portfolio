ea1e8d246a7bde36f11f08557ebab894ad52e9d6
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-11-26 18:24:45 -0800

    WIP

--- client/src/components/resume/Resume.js
+++ client/src/components/resume/Resume.js
@@ -6,18 +6,9 @@ import krystleResume from '../../assets/krystleResume.pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 const Resume = () => {
-  const [numPages, setNumPages] = useState(null)
-  const [pageNumber, setPageNumber] = useState(1)
-
-  const loadDocSuccess = ({ numPages }) => {
-    setNumPages(numPages)
-    setPageNumber(1)
-  }
-  //   const { pdf } = props
-
   return (
     <div className=" resumeDoc">
-      <Document file={krystleResume} loadSuccess={loadDocSuccess}>
+      <Document file={krystleResume}>
         <Page pageNumber={1} />
       </Document>
     </div>
